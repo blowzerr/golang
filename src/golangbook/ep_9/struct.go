b9e3df3799e1eb590cdc47f7f47bc90a5e4257b1
Alex Chuikov <blowzerr@gmail.com>
2018-03-27 14:20:32 +0300

    Structs in GO

---
+++ src/golangbook/ep_9/struct.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Circle is cool
+type Circle struct {
+	x, y, r float64
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+//Rectangle is rectangle structure
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+type Shape interface {
+	area() float64
+}
+
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
+}
+
+func main() {
+	b := Circle{1, 3, 10}
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(totalArea(&b, &r))
+}
